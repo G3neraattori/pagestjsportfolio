b061ed1c1d85992fbaea312c7c0d852d18d4f597
g3neraattori <sami.toivanen@elisanet.fi>
2022-07-27 22:10:25 +0300

    dist

---
+++ dist/desktop.js
@@ -0,0 +1,9 @@
+function showGit() {
+    const git = document.getElementById("gitPortfolio");
+    if (git.style.display === "none") {
+        git.style.display = "flex";
+    } else {
+        git.style.display = "none";
+    }
+}
+
